4327af22e1e9a5fcb1dab6c5ee8b190bd11cc106
Daniel Lawrence <64.74.6c@gmail.com>
2016-07-28 16:00:47 -0400

    Added Trace.cs, a wrapper for Debug.Log that allows for easy stripping of logging for performance testing

---
+++ Assets/Scripts/Trace.cs
@@ -0,0 +1,12 @@
+﻿#define USE_LOGS
+using UnityEngine;
+using System;
+using System.Diagnostics;
+
+public class Trace {
+
+	[Conditional("USE_LOGS")]
+	public static void Msg (string msg) {
+		UnityEngine.Debug.Log (msg);
+	}
+}
