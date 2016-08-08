56d49eda1a84c20da32d38e742752a9e17fa6644
Daniel Lawrence <64.74.6c@gmail.com>
2016-08-08 12:03:20 -0400

    Scroll locking now works correctly

--- Assets/Scripts/HistoryPane.cs
+++ Assets/Scripts/HistoryPane.cs
@@ -10,6 +10,8 @@ public class HistoryPane : MonoBehaviour {
 	private float maxHeight;
 
 	private Vector3 lastMousePosition;
+	private Vector3 startPosition;
+	private Vector3 unclampedPos;
 
 	void Start () {
 		numGuesses = 0;
@@ -18,11 +20,17 @@ public class HistoryPane : MonoBehaviour {
 	// initialize, give size of each element for scroll locking and
 	// take position when called as implicit min height
 	public void Init (float yDelta, float windowHeight) {
+		startPosition = transform.position;
 		this.yDelta = yDelta;
 		this.winHeight = windowHeight;
+		this.minHeight = startPosition.y;
 	}
 
+	// reset max height to
+	// bottomOfWindow + yDelta*numGuesses
 	public void Resize (int numGuesses) {
+		maxHeight = minHeight - (winHeight) + (yDelta * numGuesses);
+		if (maxHeight < minHeight) maxHeight = minHeight;
 		
 	}
 
@@ -33,7 +41,11 @@ public class HistoryPane : MonoBehaviour {
 	public void GetMouseDrag () {
 		float delta = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - 
 			Camera.main.ScreenToWorldPoint(lastMousePosition).y;
-		transform.Translate (0, delta, 0);
 		lastMousePosition = Input.mousePosition;
+		// clamp position
+		unclampedPos = transform.position;
+		unclampedPos.y += delta;
+		unclampedPos.y = Mathf.Clamp(unclampedPos.y,minHeight,maxHeight);
+		transform.position = unclampedPos;
 	}
 }
