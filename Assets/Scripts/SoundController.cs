d0d2d11ad1ca5177475c91f1108a58c66fa11115
jmayhew4 <heymayhew@gmail.com>
2017-08-22 00:46:34 -0400

    Added SoundController script

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundController : MonoBehaviour {
+
+	private AudioSource audioSource;
+	//public AudioClip sample;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void PlayDogBarkSound () {
+        //audioSource.PlayOneShot(sample, 0.55f);
+    }
+}
